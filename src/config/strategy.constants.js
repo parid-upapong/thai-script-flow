4fef1b5e8bd210418d5145f75d9a3749d2d6d637
tongservo <parid.upa@thaimooc.ac.th>
2026-01-14 20:55:08 +0700

    SWARM_SYNC: Update src/config/strategy.constants.js

---
+++ src/config/strategy.constants.js
@@ -0,0 +1,27 @@
+/**
+ * STRATEGIC CONSTANTS FOR THE THAI CREATIVE AI PLATFORM
+ * Defined by OVERLORD (CEO)
+ */
+
+export const STRATEGIC_PILLARS = {
+  CULTURAL_ACCURACY: 'cultural_accuracy',
+  PRODUCTION_EFFICIENCY: 'production_efficiency',
+  GLOBAL_STANDARDS: 'global_standards',
+};
+
+export const TARGET_LOCALES = ['th-TH'];
+
+export const PRODUCTION_WORKFLOW_STEPS = [
+  'Concept & Briefing',
+  'Scriptwriting (Thai Nuance)',
+  'Visual Storyboarding',
+  'Budgeting & Logistics',
+  'Talent & Location Scouting',
+  'Technical Breakdown'
+];
+
+export const SUCCESS_METRICS = {
+  TIME_SAVED_PERCENTAGE: 0.70,
+  USER_RETENTION_THAI_AGENCIES: 0.85,
+  CONTENT_QUALITY_SCORE: 'International Grade'
+};
